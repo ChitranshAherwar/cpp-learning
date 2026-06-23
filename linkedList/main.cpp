83388337f9e68457e03103fb106f2f886e104686
ChitranshAherwar <chitranshaherwar@gmail.com>
2026-06-23 18:11:14 +0530

    Add find and size operations to linked list

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -4,21 +4,30 @@ struct Node
 {
     int data;
     Node* next;
-
 };
 
-class LinkedList{
+class LinkedList
+{
 private:
     Node* head;
 
 public:
-
     LinkedList();
     ~LinkedList();
 
     void push_back(int value);
+
+    bool find(int value);
+    int size();
+
     void print_list();
 };
+
+
+LinkedList::LinkedList()
+{
+    head = nullptr;
+}
 
 LinkedList::~LinkedList()
 {
@@ -34,14 +43,9 @@ LinkedList::~LinkedList()
 
         current = current->next;
         delete temp;
-
     }
 }
 
-LinkedList::LinkedList()
-{
-    head = nullptr;
-}
 
 void LinkedList::push_back(int value)
 {
@@ -49,45 +53,89 @@ void LinkedList::push_back(int value)
     newNode->data = value;
     newNode->next = nullptr;
 
-    if(head == nullptr){
-
+    if(head == nullptr)
+    {
         head = newNode;
+    }
+    else
+    {
+        Node* current = head;
 
-    }else{
-
-    Node* current = head;
-
-    while(current->next != nullptr)
+        while(current->next != nullptr)
         {
             current = current->next;
         }
 
-            current->next = newNode;
+        current->next = newNode;
+    }
+}
+
+
+bool LinkedList::find(int value)
+{
+    Node* current = head;
+
+    while(current != nullptr)
+    {
+        if(value == current->data)
+        {
+            return true;
         }
 
+        current = current->next;
+    }
+
+    return false;
 }
+
+int LinkedList::size()
+{
+    Node* current = head;
+    int counter{0};
+
+    while(current != nullptr)
+    {
+        counter++;
+        current = current->next;
+    }
+
+    return counter;
+}
+
 
 void LinkedList::print_list()
 {
-        Node* current = head;
+    Node* current = head;
 
     while(current != nullptr)
     {
         std::cout << current->data << '\n';
-        current = current->next; // current->next means *(current).next (*ptr).member
+        current = current->next;
     }
 }
+
 
 int main()
 {
     LinkedList list;
-    
-   list.push_back(10);
-   list.push_back(20);
-   list.push_back(30);
-   list.push_back(40);
 
-   list.print_list();
+    list.push_back(10);
+    list.push_back(20);
+    list.push_back(30);
+    list.push_back(40);
+
+    if(list.find(20))
+    {
+        std::cout << "Found!\n";
+    }
+    else
+    {
+        std::cout << "Not Found!\n";
+    }
+
+    std::cout << "Size: " << list.size() << '\n';
+
+    list.print_list();
 
     return 0;
 }
